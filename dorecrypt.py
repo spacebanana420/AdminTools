18a7c90f64f5b7b6da68232f84b8d42bee9146c5
Space Banana <tomasbessa@protonmail.com>
2023-01-17 18:33:15 +0000

    Update dorecrypt.py

--- dorecrypt.py
+++ dorecrypt.py
@@ -1,7 +1,10 @@
 import os, math, secrets, sys
 
 def encrypt_openssl(operation, file): #requires checking if its a folder
-    password = input("Input password ")
+    if os.path.isdir(file) == True:
+        os.system("zip -r -3 -v '" + file + ".zip' '" + file + "'")
+        file = file + ".zip"
+    password = input("Input password: ")
     if operation == 1:
         command = 'openssl enc -e -in ' + file + ' -k ' + password + ' -pbkdf2 -aes256 -out ' + file + '.doremy'
     else:
@@ -19,18 +22,19 @@ def encrypt_zip(operation, file):
 #Main
 if len(sys.argv) >= 2:
     if os.path.exists(sys.argv[1]) == False:
-        file = input("Input a file name ")
+        file = input("Input a file name: ")
     else:
         file = sys.argv[1]
 else:
-    file = input("Input a file name ")
+    file = input("Input a file name: ")
 if os.path.exists(file) == False:
     print("The specified file/directory does not exist")
 else:
     print("1: Encrypt (openssl)     2: Decrypt (openssl)"); print("3: Encrypt (zip)     4: Decrypt (zip)")
-    operation = int(input("Choose an operation or type 0 to exit"))
+    operation = int(input("Choose an operation or type 0 to exit: "))
     match operation:
-        case 0: break;
-        case 1 or 2: encrypt_openssl(operation, file);;
-        case 3 or 4: encrypt_zip(operation, file);
-        case other: print("You need to choose an operation!");
+        case 1 | 2: encrypt_openssl(operation, file);
+        case 3 | 4: encrypt_zip(operation, file);
+        case other:
+            if operation != 0:
+                print("You need to choose an operation!");
